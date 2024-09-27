414616d54a31ae72f8d4ae81218533f639c229d7
Anushreem-04 <146901782+Anushreem-04@users.noreply.github.com>
2024-09-27 20:03:39 +0530

    Day-5 Assignment

---
+++ day5 Java Assignment/src/Q1RegistrationForm.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q1RegistrationForm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.println("Enter your Country: ");
+        String country = sc.next();
+        System.out.println("Welcome "+name+".Your age is "+age+"and you are from "+country);
+        sc.close();
+    }
+}
---
+++ day5 Java Assignment/src/Q4SignedByte.java
@@ -0,0 +1,8 @@
+public class Q4SignedByte {
+    public static void main(String[] args) {
+        byte number=125;
+        System.out.println("Value of number: "+number);
+        number=127;
+        System.out.println("lrgest value stored in a signed byte: "+number);
+    }
+}
